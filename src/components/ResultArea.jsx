75036cf3d8790a232261a9852c8237920338ebc8
Woori Kang <rose5307@naver.com>
2023-04-29 21:51:09 +0900

    230429 complete main function.(except input error handling)

--- src/components/ResultArea.jsx
+++ src/components/ResultArea.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const ResultArea = ({ date }) => {
   const { day, month, year } = date;
   const today = new Date();
+  let [rDay, setDay] = useState(0);
+  let [rMonth, setMonth] = useState(0);
+  let [rYear, setYear] = useState(0);
   const getAge = () => {
     const temp = new Date(`${year}-${month - 1}-${day}`);
-    let rDay = Math.floor((today - temp) / (1000 * 60 * 60 * 24));
-    let rMonth = Math.floor(rDay / 30);
-    let rYear = Math.floor(rMonth / 12);
-    rMonth = rMonth % 12;
-    rDay = rDay % 30;
+    let tDay = Math.floor((today - temp) / (1000 * 60 * 60 * 24));
+    let tMonth = Math.floor(tDay / 30);
+    let tYear = Math.floor(tMonth / 12);
+    tMonth = tMonth % 12;
+    tDay = tDay % 30;
 
-    console.log(temp);
-    console.log(today);
-    console.log(rYear);
-    console.log(rMonth);
-    console.log(rDay);
+    setDay(tDay);
+    setMonth(tMonth);
+    setYear(tYear);
+    
   };
+  console.log(isNaN(rYear))
   useEffect(() => {
     getAge();
   }, [date]);
   return (
     <div>
-      <p>{year}Years</p>
-      <p>{month}Months</p>
-      <p>{day}Days</p>
+      <p>{isNaN(rYear) ? '--' : rYear}Years</p>
+      <p>{isNaN(rMonth) ? '--' : rMonth}Months</p>
+      <p>{isNaN(rDay) ? '--' : rDay}Days</p>
     </div>
   );
 };
