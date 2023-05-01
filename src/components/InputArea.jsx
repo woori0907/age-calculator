4c7e39ac0d7f673b4781d3544d6fe2312779a711
Woori Kang <rose5307@naver.com>
2023-05-01 23:09:27 +0900

    230501 css additional setting and input area design

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import styles from "./InputArea.module.css";
 
 const InputArea = ({ date, setDate }) => {
   const inputDay = useRef();
@@ -26,31 +27,37 @@ const InputArea = ({ date, setDate }) => {
   };
   return (
     <div>
-      <section>
-        <form onSubmit={handleFormSubmit}>
-          <label htmlFor="input__day">DAY</label>
-          <input
-            id="input__day"
-            type="number"
-            placeholder="DD"
-            ref={inputDay}
-          />
-
-          <label htmlFor="input__month">Month</label>
-          <input
-            id="input__month"
-            type="number"
-            placeholder="MM"
-            ref={inputMonth}
-          />
-
-          <label htmlFor="input__year">YEAR</label>
-          <input
-            id="input__year"
-            type="number"
-            placeholder="YEAR"
-            ref={inputYear}
-          />
+      <section className={styles.input_form_wrap}>
+        <form onSubmit={handleFormSubmit} className={styles.input_form}>
+          <div className={styles.input_field_wrap}>
+            <div className={styles.input_field_item}>
+              <label htmlFor="input__day">DAY</label>
+              <input
+                id="input__day"
+                type="number"
+                placeholder="DD"
+                ref={inputDay}
+              />
+            </div>
+            <div className={styles.input_field_item}>
+              <label htmlFor="input__month">Month</label>
+              <input
+                id="input__month"
+                type="number"
+                placeholder="MM"
+                ref={inputMonth}
+              />
+            </div>
+            <div className={styles.input_field_item}>
+              <label htmlFor="input__year">YEAR</label>
+              <input
+                id="input__year"
+                type="number"
+                placeholder="YEAR"
+                ref={inputYear}
+              />
+            </div>
+          </div>
           <input type="submit" value="Submit" />
         </form>
       </section>
